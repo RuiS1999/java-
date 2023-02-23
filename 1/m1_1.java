b7462e99c516571a0015612e8db70d2a52284ca4
EC2 Default User <ec2-user@ip-172-31-7-97.ap-northeast-1.compute.internal>
2023-02-23 02:03:36 +0000

    java箱ver.2

---
+++ 1/m1_1.java
@@ -0,0 +1,12 @@
+public class m1_1 {
+  public static void main (String []args){
+    int as;
+    String sd;
+    as = 12;
+    sd = "Final";
+    System.out.println(as + as);
+    System.out.println(sd);
+    as = 100;
+    System.out.println(as*as);
+  }
+}
